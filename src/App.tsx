4ae031272576ca870c118aaddf12c2dfbf59193c
Matthew Hylton <wehttamh11@gmail.com>
2020-04-19 13:26:03 -0600

    heroku

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import './App.css';
 import Menu from './Menu';
 import Gameboard from './Gameboard';
 
-class App extends React.Component <{}, { menu: string, game: string, theme: string}> {
+class App extends React.Component <{}, { data: any, menu: string, game: string, theme: string}> {
 
 constructor(props: any) {
   super(props);
   this.state = {
+    data: null,
     menu: "active",
     game: "inactive",
     theme: "classic"
   };
 }
+
+componentDidMount() {
+this.callBackendAPI()
+  .then(res => this.setState({ data: res.express }))
+  .catch(err => console.log(err));
+}
+
+callBackendAPI = async () => {
+const response = await fetch('/');
+const body = await response.json();
+
+if (response.status !== 200) {
+  throw Error(body.message) 
+}
+return body;
+};
 
 start = (data: string) => {
   this.setState({
