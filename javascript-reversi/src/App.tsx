2f28f129ecaaab5b7941176427de3fbaa1424c97
Matthew Hylton <wehttamh11@gmail.com>
2020-04-21 23:14:53 -0600

    socket.io production /?

--- javascript-reversi/src/App.tsx
+++ javascript-reversi/src/App.tsx
@@ -5,7 +5,7 @@ import Gameboard from './Gameboard';
 import socketIOClient from 'socket.io-client'
 
 //const socket = socketIOClient("localhost:5000");
-const socket = socketIOClient("www.javascript-reversi.com/");
+const socket = socketIOClient("/");
 
 class App extends React.Component <{}, { data: any, menu: string, game: string, theme: string, user1: string, user2: string, type: string, input: string, room: string}> {
 
